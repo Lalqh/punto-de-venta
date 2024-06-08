f288b280fb174da63eb613669dbfacb1a649bc52
Lalqh <lalincotero13@gmail.com>
2024-06-07 21:31:27 -0600

    Se agrego la funcion para realizar petciones get

--- src/services/ApiServices.ts
+++ src/services/ApiServices.ts
@@ -1,4 +1,5 @@
 const API_URL = "http://localhost:3010/api/v1/";
+const token = localStorage.getItem("token") || "";
 
 export const fecthPost = async (url: string, data: any) => {
   try {
@@ -6,8 +7,28 @@ export const fecthPost = async (url: string, data: any) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(data),
+    });
+
+    return response.json();
+  } catch (error) {
+    return {
+      success: false,
+      message: "Error al realizar la petición",
+    };
+  }
+};
+
+export const fectGet = async (url: string) => {
+  try {
+    const response = await fetch(`${API_URL}${url}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
     });
 
     return response.json();
